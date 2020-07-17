537f12ad5795c96259cb72ff28564efb5ca675d7
Alex Flint <alex.flint@gmail.com>
2020-07-17 12:56:20 -0700

    factored out authentication to google and added push-google-doc.go

---
+++ google-auth.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var tok oauth2.Token
+	err = json.NewDecoder(f).Decode(&tok)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tok, nil
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	return json.NewEncoder(f).Encode(token)
+}
+
+type googleTokenSource struct {
+	ctx     context.Context
+	config  *oauth2.Config
+	tokFile string
+}
+
+func (ts googleTokenSource) Token() (*oauth2.Token, error) {
+	authURL := ts.config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		return nil, fmt.Errorf("unable to read authorization code: %w", err)
+	}
+
+	tok, err := ts.config.Exchange(ts.ctx, authCode)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving token from web: %w", err)
+	}
+
+	if ts.tokFile != "" {
+		err = saveToken(ts.tokFile, tok)
+		if err != nil {
+			return nil, fmt.Errorf("error saving token to file: %w", err)
+		}
+	}
+
+	return tok, nil
+}
+
+// GoogleAuth authenticates with Google using oauth
+func GoogleAuth(ctx context.Context, tokFile string, scopes ...string) (oauth2.TokenSource, error) {
+	// get oauth credentials
+	b, err := ioutil.ReadFile("oauth_credentials.json")
+	if err != nil {
+		return nil, fmt.Errorf("unable to read client secret file: %w", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, scopes...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
+	}
+
+	// create a token source that performs the oauth flow
+	var ts oauth2.TokenSource = googleTokenSource{
+		ctx:    ctx,
+		config: config,
+	}
+
+	// try load the token from a file
+	tok, err := tokenFromFile(tokFile)
+	if err == nil {
+		ts = oauth2.ReuseTokenSource(tok, ts)
+	}
+
+	return ts, nil
+}
---
+++ push-google-doc.go
@@ -0,0 +1,83 @@
+package main
+
+// TODO:
+//   deal with emphasizing text runs that end with whitespace
+//   deal with tables
+//   deal with equations
+//   detect code blocks
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/kr/pretty"
+	"google.golang.org/api/docs/v1"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+)
+
+type pushGoogleDocArgs struct {
+	Document string
+}
+
+func pushGoogleDoc(ctx context.Context, args *pushGoogleDocArgs) error {
+	const tokFile = ".cache/google-push-token.json"
+	googleToken, err := GoogleAuth(ctx, tokFile,
+		"https://www.googleapis.com/auth/documents",
+		"https://www.googleapis.com/auth/drive.readonly")
+
+	// create the drive client
+	driveClient, err := drive.NewService(ctx, option.WithTokenSource(googleToken))
+	if err != nil {
+		return fmt.Errorf("error creating drive client: %w", err)
+	}
+
+	revList, err := driveClient.Revisions.List(args.Document).Do()
+	if err != nil {
+		return fmt.Errorf("error getting revision list for document: %w", err)
+	}
+	pretty.Println(revList)
+	return nil
+
+	// create the docs client
+	docsClient, err := docs.NewService(ctx, option.WithTokenSource(googleToken))
+	if err != nil {
+		return fmt.Errorf("error creating docs client: %w", err)
+	}
+
+	// pull the document
+	existingDoc, err := docsClient.Documents.Get(args.Document).Do()
+	if err != nil {
+		return fmt.Errorf("error retrieving document: %w", err)
+	}
+
+	_ = existingDoc
+
+	title := existingDoc.Body.Content[1]
+	titleContent := title.Paragraph.Elements[0].TextRun.Content
+	titleLength := utf8.RuneCountInString(strings.TrimSuffix(titleContent, "\n"))
+
+	update := docs.Request{
+		InsertText: &docs.InsertTextRequest{
+			Text: " FOO",
+			Location: &docs.Location{
+				Index: title.StartIndex + int64(titleLength),
+			},
+		},
+	}
+
+	resp, err := docsClient.Documents.BatchUpdate(args.Document, &docs.BatchUpdateDocumentRequest{
+		Requests: []*docs.Request{
+			&update,
+		},
+	}).Context(ctx).Do()
+	_ = resp
+
+	if err != nil {
+		return fmt.Errorf("error updating document: %w", err)
+	}
+
+	return nil
+}
