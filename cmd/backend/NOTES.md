1b8708ea304756f14c2bb7d00abaa61e638678f4
Alex Flint <alex.flint@gmail.com>
2022-03-18 19:41:09 -0700

    backend now displays a textbox and receives its contents on backend

---
+++ cmd/backend/NOTES.md
@@ -0,0 +1,5 @@
+TODO
+- Try changing addon.json to use things other than "homepageTrigger"
+- Try putting the "request file scope" thing inline into the root card "actions"
+- Try putting "pop to root card" into the card that requests the current file scope
+- Try logging the POST data in the endpoint that requests the current file scope
--- cmd/backend/backend.go
+++ cmd/backend/backend.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/alexflint/go-arg"
 	"golang.org/x/oauth2"
@@ -18,34 +17,20 @@ import (
 	"google.golang.org/api/option"
 )
 
-const resp = `
-{
-	"action": {
-		"navigations": [{
-			"pushCard": {
-				"sections": [{
-					"widgets": [
-						{
-							"textParagraph": {
-								"text": "Hello world!"
-							}
-						}
-					]
-				}],
-				"fixedFooter": {
-					"primaryButton": {
-						"text": "Authorize file access",
-						"onClick": {
-							"action": {
-								"function": "https://doc-publisher-backend-mxh6l73c4a-uc.a.run.app/authorizeFile"
-							}
-						}
-					}
-				}
-			}
-		}]
-	}
-}`
+//go:embed cards/root.json
+var rootCard []byte
+
+//go:embed cards/demo.json
+var demoCard []byte
+
+//go:embed cards/requestAccess.json
+var requestAccess []byte
+
+//go:embed cards/accessGranted.json
+var accessGranted []byte
+
+//go:embed cards/textChanged.json
+var textChanged []byte
 
 // represents the JSON payload sent to us by Google Workspace
 type workspaceAuthorization struct {
@@ -73,35 +58,118 @@ type driveEventObject struct {
 
 type workspacePayload struct {
 	Authorization workspaceAuthorization `json:"authorizationEventObject"`
+	Common        commonEventObject      `json:"commonEventObject"`
 }
 
-func handleAuthorizeFile(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`{
-		"renderActions": {
-			"hostAppAction": {
-				"editorAction": {
-					"requestFileScopeForActiveDocument": {}
-				}
-			}
-		}
-	}`))
+type commonEventObject struct {
+	HostApp    string               `json:"hostApp"`  // "DOCS", "GMAIL", "CALENDAR"
+	Platform   string               `json:"platform"` // "WEB"
+	FormInputs map[string]formInput // Input name -> value
+}
+
+// formInput models the data that Workspaces sends us when a form is submitted. There
+// is one of these for each widget.
+type formInput struct {
+	StringInputs stringInputs `json:"stringInputs"`
+}
+
+// stringInputs models the data the Workspaces sends us for a textbox when a form is
+// submitted. I don't know why "value" is an array or in what cases it would have other
+// than one element.
+type stringInputs struct {
+	Value []string `json:"value"`
+}
+
+// invoked when the user edits the "document ID" textbox in the root card
+// renders nothing
+func handleTextChanged(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
+		log.Printf("error reading request body: %v", err)
+		return
+	}
+
+	var payload workspacePayload
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		http.Error(w, "error parsing body as JSON", http.StatusBadRequest)
+		log.Printf("error parsing body as JSON: %v", err)
+		return
+	}
+
+	if x, ok := payload.Common.FormInputs["Document ID"]; ok && len(x.StringInputs.Value) == 1 {
+		docID := x.StringInputs.Value[0]
+		log.Println("##### textChanged received document ID: " + docID)
+	}
+
+	log.Println("received at textChanged: ", string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(textChanged)
+}
+
+// invoked when the user clicks "authorize" on the root card
+// renders a card that asks the user to give this addon access to the google doc they have open
+func handleRequestAccess(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
+		log.Printf("error reading request body: %v", err)
+		return
+	}
+
+	log.Println("received at requestAccess: ", string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(requestAccess)
+}
+
+// invoked when the user grants this addon access to the google doc they have open
+// renders an empty card
+func handleAccessGranted(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
+		log.Printf("error reading request body: %v", err)
+		return
+	}
+
+	log.Println("received at accessGranted: ", string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(accessGranted)
+}
+
+func handleDemo(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
+		log.Printf("error reading request body: %v", err)
+		return
+	}
+
+	log.Println("received at demo: ", string(body))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(demoCard)
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("error reading request body: %v", err), http.StatusInternalServerError)
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
 		log.Printf("error reading request body: %v", err)
 		return
 	}
 
-	log.Println(string(body))
+	log.Println("received at root: ", string(body))
 
 	var payload workspacePayload
 	err = json.Unmarshal(body, &payload)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("error parsing body: %v", err), http.StatusBadRequest)
-		log.Printf("error parsing body: %v", err)
+		http.Error(w, "error parsing body as JSON", http.StatusBadRequest)
+		log.Printf("error parsing body as JSON: %v", err)
 		return
 	}
 
@@ -110,7 +178,8 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	http.ServeContent(w, r, "response.json", time.Time{}, strings.NewReader(resp))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(rootCard)
 }
 
 func tryStuff(ctx context.Context, payload *workspacePayload) error {
@@ -169,7 +238,10 @@ func main() {
 	}
 	arg.MustParse(&args)
 
-	http.Handle("/authorizeFile", http.HandlerFunc(handleAuthorizeFile))
+	http.Handle("/textChanged", http.HandlerFunc(handleTextChanged))
+	http.Handle("/accessGranted", http.HandlerFunc(handleAccessGranted))
+	http.Handle("/requestAccess", http.HandlerFunc(handleRequestAccess))
+	http.Handle("/demo", http.HandlerFunc(handleDemo))
 	http.Handle("/", http.HandlerFunc(handleRoot))
 
 	// we must add the colon ourselves because Cloud Run will give us an integer port
